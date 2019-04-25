36f3c2e98614c1a72f2a1a7dede3626142d96dfb
xiaoxuaoj <35458234+xiaoxuaoj@users.noreply.github.com>
2019-04-25 22:20:52 +0800

    lab4

---
+++ TestBackPack.java
@@ -0,0 +1,21 @@
+﻿import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestBackPack {
+	private BackPack backpack;
+	@Test
+	public void testSolution() {
+		int result[][] = new int[][]{{0,0,0,0,0,0,0,0,0,0,0},
+						  {0,0,0,4,4,4,4,4,4,4,4},
+						  {0,0,0,4,5,5,5,9,9,9,9},
+						  {0,0,0,4,5,6,6,9,10,11,11}};
+		int m = 10;
+                int n = 3;
+        /*
+        放入的三个物体重量分别为3， 4， 5；价值分别为4， 5， 6
+         */
+        int w[] = {3, 4, 5};
+        int p[] = {4, 5, 6};
+        assertEquals(result, backpack.BackPack_Solution(m, n, w, p));
+	}
+}
